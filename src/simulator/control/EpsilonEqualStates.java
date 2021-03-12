99094635b462926ba4255db54c9ba5cde3382a2c
Felix <Felix@Jeez>
2021-03-12 16:58:53 +0100

    massEqual

--- src/simulator/control/EpsilonEqualStates.java
+++ src/simulator/control/EpsilonEqualStates.java
@@ -1,13 +1,62 @@
 package simulator.control;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import simulator.misc.Vector2D;
 
 public class EpsilonEqualStates implements StateComparator {
 
+	private double eps;
+	
+	//Constructora
+	EpsilonEqualStates(double eps){
+		this.eps = eps;
+	}
+	
 	@Override
 	public boolean equal(JSONObject s1, JSONObject s2) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		//Comparo que sus claves "time" son iguales
+		double s1time = s1.getDouble("time");
+		double s2time = s1.getDouble("time");
+		if(Math.abs(s1time-s2time) > this.eps) { //Sus tiempos no son iguales modulo epsilon
+			return false;
+		}
+		
+		JSONArray ja1 = s1.getJSONArray("bodies");
+		JSONArray ja2 = s2.getJSONArray("bodies");
+		if(ja1.length() != ja2.length()) return false;
+		for(int i =0; i < ja1.length(); i++) { //Para cada cuerpo, comparo sus vectores
+			//Compruebo que los objetos i-esimos tienen el mismo id
+			String id1 = (String) ja1.getJSONObject(i).get("id");
+			String id2 = (String) ja2.getJSONObject(i).get("id");
+			if( !id1.equals(id2)) return false;
+		
+			//Compruebo que sus masas son iguales modulo epsilon
+			double m1 = (double) ja1.getJSONObject(i).get("m");
+			double m2 = (double) ja2.getJSONObject(i).get("m");
+			if( Math.abs(m1-m2) > this.eps) return false;
+			
+			//Compruebo que sus vectores posicion son iguales modulo epsilon
+			Vector2D v1 = new Vector2D( (Vector2D) ja1.getJSONObject(i).get("p") );
+			Vector2D v2 = new Vector2D( (Vector2D) ja2.getJSONObject(i).get("p") );
+			if( v1.distanceTo(v2) > this.eps) return false;
+			
+			//Compruebo que sus vectores velocidad son iguales modulo epsilon
+			Vector2D ve1 = new Vector2D( (Vector2D) ja1.getJSONObject(i).get("v") );
+			Vector2D ve2 = new Vector2D( (Vector2D) ja2.getJSONObject(i).get("v") );
+			if( ve1.distanceTo(ve2) > this.eps) return false;
+
+			//Compruebo que sus vectores fuerza son iguales modulo epsilon
+			Vector2D f1 = new Vector2D( (Vector2D) ja1.getJSONObject(i).get("f") );
+			Vector2D f2 = new Vector2D( (Vector2D) ja2.getJSONObject(i).get("f") );
+			if( f1.distanceTo(f2) > this.eps) return false;
+
+
+		}
+		
+		return true;
 	}
 
 }
--- src/simulator/control/MassEqualStates.java
+++ src/simulator/control/MassEqualStates.java
@@ -1,13 +1,24 @@
 package simulator.control;
 
+import java.util.List;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import simulator.model.Body;
 
 public class MassEqualStates implements StateComparator {
 
 	@Override
 	public boolean equal(JSONObject s1, JSONObject s2) {
-		// TODO Auto-generated method stub
-		return false;
+
+		if (s1.get("tiempo_actual") == s2.get("tiempo_actual")) {
+			List<Body> bodies_s1 = (List<Body>) s1.get("bodies");
+			List<Body> bodies_s2 = (List<Body>) s2.get("bodies");
+			
+			return true;
+		} else
+			return false;
 	}
 
 }
