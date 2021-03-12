dd45c8da04069da54e5db95d7882fc7c88de5485
Felix <Felix@Jeez>
2021-03-12 17:53:18 +0100

    MassEqualStates

--- src/simulator/control/MassEqualStates.java
+++ src/simulator/control/MassEqualStates.java
@@ -15,7 +15,13 @@ public class MassEqualStates implements StateComparator {
 		if (s1.get("tiempo_actual") == s2.get("tiempo_actual")) {
 			List<Body> bodies_s1 = (List<Body>) s1.get("bodies");
 			List<Body> bodies_s2 = (List<Body>) s2.get("bodies");
-			
+
+			for (int i = 0; i < bodies_s1.size(); i++) {
+				if (bodies_s1.get(i).getId() != bodies_s2.get(i).getId()
+						|| bodies_s1.get(i).getMass() != bodies_s2.get(i).getMass())
+					return false;
+			}
+				// el bucle ha acabado todos son iguales.
 			return true;
 		} else
 			return false;
