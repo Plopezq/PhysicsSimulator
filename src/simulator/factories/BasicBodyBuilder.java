a88525af1cb28cfb6d1ca3a97f0fc25c1f2e2bba
Pablo López Martín <lopezmartinpablo@gmail.com>
2021-03-24 17:43:32 +0100

    Try to do builder and BasicBodyBuilder

--- src/simulator/factories/BasicBodyBuilder.java
+++ src/simulator/factories/BasicBodyBuilder.java
@@ -1,5 +1,19 @@
 package simulator.factories;
 
-public class BasicBodyBuilder {
+import org.json.JSONObject;
 
+import simulator.model.Body;
+
+public class BasicBodyBuilder extends Builder<Body>{
+
+	private Body cuerpo;
+	BasicBodyBuilder(){
+		
+	}
+	
+	protected Body createTheInstance(JSONObject info){
+		//PROBAR que alomejor toca poner: info.get("type".get("id"))
+		this.cuerpo = new Body(info.get("id"),info.get("v"), info.get("p") ,info.get("m"));
+		return this.cuerpo;
+	}
 }
--- src/simulator/factories/Builder.java
+++ src/simulator/factories/Builder.java
@@ -1,5 +1,67 @@
 package simulator.factories;
 
+import org.json.JSONObject;
+
 public abstract class Builder<T> {
 
+	
+	private T objeto;
+	Builder(){
+		
+	}
+	
+	public T createInstance(JSONObject info) {
+		//si la información suministrada por info es correcta, 
+		//entonces crea un objeto de tipo T
+		//En otro caso devuelve null para indicar 
+		//que este constructor es incapaz de reconocer ese formato.
+		/* En caso de que reconozca el campo type pero haya un error 
+		 * en alguno de los valores suministrados por la sección data, 
+		 * el método lanza una excepcion IllegalArgumentException.*/
+	
+		//Comprobamos que el campo type del JSON es correcto
+		String aux = (String) info.get("type");
+		if(aux.equals("basic")) {
+			return (T) new BasicBodyBuilder().createInstance((JSONObject) info.get("type"));
+		}else if(aux.equals("mlb")) {
+			
+		}else if(aux.equals("nlug")) {
+			
+		}else if(aux.equals("mtcp")) {
+			
+		}else if(aux.equals("nf")) {
+			
+		}else if(aux.equals("masseq")) {
+			
+		}else if(aux.equals("epseq")) {
+			
+		}else {
+			//No hay ningun objeto de ese tipo
+			return null;
+		}
+		
+		
+		//Comprobamos los valores de la seccion data son correctos --> eso se hara en el propio objeto
+		
+		
+		
+		
+		
+		return null;
+	}
+	
+	public JSONObject getBuilderInfo() {
+		
+		/* devuelve un objeto JSON que sirve de plantilla para el 
+		 * correspondiente constructor, i.e., un valor válido para el parámetro de 
+		 * createInstance (ver getInfo() de Factory<T>).*/
+		
+		
+		
+		
+		return null;
+	}
+	
+	
+	
 }
