1b92b05718c82f911b5fb7d58a9ee2204da935f5
Felix <Felix@Jeez>
2021-04-07 23:25:29 +0200

    Body & NewtonUniversalGravitotion
    Cambios en body y en la ley universal. (muchos, creo) sigue sin
    funcionar del todo, pero he hecho las operaciones a mano y corresponden
    con nuestros resultados .. no se muy bien cual es el problema, le
    mandare un mail a la profesora

--- src/simulator/model/Body.java
+++ src/simulator/model/Body.java
@@ -1,109 +1,126 @@
 package simulator.model;
 
-
 import org.json.JSONObject;
 
 import simulator.misc.Vector2D;
 
 public class Body {
-	
-	//Atributos
-	protected String id;	// Identificador
+
+	// Atributos
+	protected String id; // Identificador
 	protected Vector2D v; // Vector velocidad
 	protected Vector2D f; // Vector de fuerza
 	protected Vector2D p; // Vector de posicion
 	protected double m; // Masa del cuerpo
-	
-	//Constructora
-	public Body(String id,Vector2D v, Vector2D p, Double m ){
+
+	// Constructora
+	public Body(String id, Vector2D v, Vector2D p, Double m) {
 		this.id = id;
 		this.v = v;
 		this.p = p;
 		this.m = m;
 		this.f = new Vector2D(); // [0,0]
 	}
-	
-	//Metodos
-	public String getId() { //devuelve el identiﬁcador del cuerpo.
+
+	// Metodos
+	public String getId() { // devuelve el identiﬁcador del cuerpo.
 		return this.id;
 	}
-	public Vector2D getVelocity() { //devuelve el vector de velocidad.
+
+	public Vector2D getVelocity() { // devuelve el vector de velocidad.
 		return this.v;
 	}
+
 	public void setVelocity(Vector2D v) {
 		this.v = v;
 	}
+
 	public Vector2D getForce() {// devuelve el vector de fuerza.
 		return this.f;
 	}
+
 	public Vector2D getPosition() {// devuelve el vector de posición.
 		return this.p;
 	}
-	public double getMass() { //devuelve la masa del cuerpo.
+
+	public double getMass() { // devuelve la masa del cuerpo.
 		return this.m;
 	}
-	//PACKAGE PROTECTED
+
+	// PACKAGE PROTECTED
 	void addForce(Vector2D f) {
-	//añade la fuerza f al vector de fuerza del cuerpo 
-		//(usando el método plus de la clase Vector2D).
+		// añade la fuerza f al vector de fuerza del cuerpo
+		// (usando el método plus de la clase Vector2D).
 		this.f = this.f.plus(f);
 	}
-	//PACKAGE PROTECTED
-	void resetForce() { //pone el valor del vector de fuerza a (0, 0).
-		this.f = new Vector2D(); //		_x = _y = 0.0;
+
+	// PACKAGE PROTECTED
+	void resetForce() { // pone el valor del vector de fuerza a (0, 0).
+		this.f = new Vector2D(); // _x = _y = 0.0;
 	}
+
 	void move(double t) { // mueve el cuerpo durante t segundos utilizando los atributos del mismo.
-		//VER pagina 4 abajo
-			//	1.Calculamos la aceleracion usando las leyes de newton--> aceleracion = fuerza/masa
-			System.out.println("FUERZA");	
-			System.out.println(this.f);
-			Vector2D a = this.f.scale(1/this.m);
-			//System.out.println(a);
-			if(this.m == 0) {
-				a = new Vector2D(); //pongo la aceleracion a 0 si la masa es 0
-			}else {
-				// 2.Cambiamos la posicion a p + v*t + 1/2 * a *t2
-				Vector2D aux1 = this.v.scale(t); // v * t
-				//System.out.println(t);
-				double t2 = t*t; //t2
+		// VER pagina 4 abajo
+		// 1.Calculamos la aceleracion usando las leyes de newton--> aceleracion =
+		// fuerza/masa
+		// System.out.println("FUERZA");
+		// System.out.println(this.f);
+		System.out.println("f: " + f);
+		System.out.println("m: " + m);
+		System.out.println("v: " + v);
 
-				Vector2D aux2 = a.scale(0.5); // 1/2 * a
-				//System.out.println(aux2);
+		Vector2D a = new Vector2D(); // pongo la aceleracion a 0 si la masa es 0
 
-				Vector2D aux3 = aux2.scale(t2);
-				
-				this.p = this.p.plus(aux1);
-				this.p = this.p.plus(aux3);
+		if (this.m != 0) {
+			a = this.f.scale(1 / this.m);
+		}
+		// System.out.println(a);
 
-				//y la velocidad a v + a*t
-				this.v = this.v.plus(a.scale(t));
-			}
+		// 2.Cambiamos la posicion a p + v*t + 1/2 * a *t2
+		Vector2D vt = this.v.scale(t); // v * t
+		System.out.println("VT: " + vt);
+
+		this.p = this.p.plus(vt);
+		System.out.println("PVT: " + p);
+
+		// System.out.println(t);
+		double t2 = t * t; // t2
+
+		Vector2D aux2 = a.scale(1.0 / 2.0); // 1/2 * a
+		System.out.println(aux2);
+
+		Vector2D aux3 = aux2.scale(t2);
+
+		this.p = this.p.plus(aux3);
+		System.out.println("P: " + p);
+
+		// y la velocidad a v + a*t
+		this.v = this.v.plus(a.scale(t));
+		System.out.println("a: " + a);
+		System.out.println("v: " + v);
 
 	}
+
 	public JSONObject getState() {
-		//{ “id": id, "m": m, "p": p~, "v": ~v , "f": f }
+		// { “id": id, "m": m, "p": p~, "v": ~v , "f": f }
 		JSONObject stateJSON = new JSONObject();
 		stateJSON.put("id", this.getId());
 		stateJSON.put("m", this.getMass());
-		stateJSON.put("p", this.getPosition().toString());
-		stateJSON.put("v", this.getVelocity().toString());
-		stateJSON.put("f", this.getForce().toString());
+		stateJSON.put("p", this.getPosition().asJSONArray());
+		stateJSON.put("v", this.getVelocity().asJSONArray());
+		stateJSON.put("f", this.getForce().asJSONArray());
 
-		
-	/*Maybe we have to do like this : 
-	 * TODO
-	 * // we put some keys with simple values into 'jo1'
-		jo1.put("a", 1234);
-		jo1.put("b", 123.3e-10);
-		jo1.put("c", "Hollaaa");
-	 */
-		return stateJSON;	
+		/*
+		 * Maybe we have to do like this : TODO // we put some keys with simple values
+		 * into 'jo1' jo1.put("a", 1234); jo1.put("b", 123.3e-10); jo1.put("c",
+		 * "Hollaaa");
+		 */
+		return stateJSON;
 	}
-	
-	public String toString() { //devuelve getState().toString().
+
+	public String toString() { // devuelve getState().toString().
 		return this.getState().toString();
 	}
-	
 
 	@Override
 	public int hashCode() {
@@ -129,6 +146,5 @@ public class Body {
 			return false;
 		return true;
 	}
-
 
 }
--- src/simulator/model/NewtonUniversalGravitation.java
+++ src/simulator/model/NewtonUniversalGravitation.java
@@ -1,52 +1,52 @@
 package simulator.model;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import org.json.JSONObject;
 
 import simulator.misc.Vector2D;
 
 public class NewtonUniversalGravitation implements ForceLaws {
 
 	private double G;
-	
+
 	public NewtonUniversalGravitation(double G) {
 		this.G = G;
 	}
+
 	public NewtonUniversalGravitation() {
 		this.G = 6.67E-11;
 	}
-	
 
 	@Override
 	public void apply(List<Body> bs) {
-			//Dos cuerpos se aplican una fuerza, uno sobre otro --> dos bucles for
-		for(int i = 0; i < bs.size(); i++) {
-			Vector2D Fi = new Vector2D(0,0); //Sumatorio de la fuerza que aplican todos los cuerpos j sobre i
-			for(int j = 0; j < bs.size(); j++) {
-				if(i != j) { //NO se aplica la fuerza sobre si mismo
-					if(bs.get(i).m == 0) { //si la masa de i es 0, la aceleracion y la velocidad valen 0
-						bs.get(i).setVelocity(new Vector2D());
-					}else {//Si la masa NO es 0, calculamos Fi
-						double denominador = bs.get(j).getPosition().distanceTo(bs.get(i).getPosition());
-						if(denominador == 0) {
-							double fij = 0;
-						}
-						if(denominador > 0){
+		// Dos cuerpos se aplican una fuerza, uno sobre otro --> dos bucles for
+
+		for (int i = 0; i < bs.size(); i++) {
+			Vector2D Fi = new Vector2D(); // Sumatorio de la fuerza que aplican todos los cuerpos j sobre i
+			if (bs.get(i).m == 0) // si la masa de i es 0, la aceleracion y la velocidad valen 0
+				bs.get(i).setVelocity( new Vector2D());
+			else
+				for (int j = 0; j < bs.size(); j++) {
+					if (i != j) { // NO se aplica la fuerza sobre si mismo
+						double distancia = bs.get(j).getPosition().distanceTo(bs.get(i).getPosition());
+						double fij = 0;
+						if (distancia > 0) {
 							double numerador = bs.get(i).getMass() * bs.get(j).getMass();
-							Vector2D dist = bs.get(i).getPosition().minus(bs.get(j).getPosition());
-							double fij = this.G * (numerador)/(Math.pow(denominador, 2));
-							Vector2D Fij = new Vector2D(dist.scale(fij));
-							Fi = Fi.plus(Fij); //Le sumo la fuerza que se le aplican
+							
+							fij = (this.G * numerador) / (Math.pow(distancia, 2)); 
+							Vector2D dij = new Vector2D(bs.get(j).p.minus(bs.get(i).p));
+							Vector2D Fij = new Vector2D(dij.scale(fij));
+
+							Fi = Fi.plus(Fij); // Le sumo la fuerza que se le aplican
+							
 						}
 					}
 				}
-			}
-			//En caso de ser el mismo, se sumaria 0,0
-			bs.get(i).addForce(Fi); //Modifico su aceleracion
+			// En caso de ser el mismo, se sumaria 0,0
+			bs.get(i).addForce(Fi); // Modifico su aceleracion
+
 		}
 	}
+
 	@Override
 	public String toString() {
 
